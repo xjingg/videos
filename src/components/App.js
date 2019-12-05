c510558c8ddd249348490801da068c0716640aff
xbloo <junecug@gmail.com>
2019-12-04 19:40:18 -0500

    Initial Commit

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import SearchBox from './SearchBox';
+
+function App() {
+  return (
+    <div className="App">
+     <SearchBox />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/SearchBox.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+class SearchBox extends React.Component{
+    state={term: ''};
+    
+    onInputChange = event=>{
+        this.setState({term: event.target.value});
+    };
+
+    onFormSubmit=event=>{
+        event.preventDeafult();
+
+    }
+
+    render(){
+        return(
+            <div className="search-bar ui segment">
+                <form pnSubmit={this.onFormSubmit} className="ui form">
+                    <input
+                    type="text"
+                    value={this.onInputChange} />
+                    </form>
+                </div>
+        )
+    }
+}
+
+
+export default SearchBox;
